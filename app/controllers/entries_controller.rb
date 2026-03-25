fc841d0e5d66660ca070c4597ebdfcc653a78ec0
nanadonkor <nana.donkor@solirius.com>
2026-03-25 15:42:39 +0000

    Validate documentation links before rendering

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -22,6 +22,7 @@ class EntriesController < ApplicationController
 
   def show
     @entry = Entry.find(params[:id])
+    @safe_doc_link = safe_doc_link(@entry.doc_link)
   end
 
   def destroy
@@ -42,6 +43,18 @@ class EntriesController < ApplicationController
   end
 
   private
+
+  def safe_doc_link(url)
+  return nil if url.blank?
+
+  uri = URI.parse(url)
+
+  return url if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+
+  nil
+rescue URI::InvalidURIError
+  nil
+end
 
   def entry_params
     params.require(:entry).permit(:technology, :use_case)
